bc3b354e5b970fa0b34af78ccfee246615c221ad
Guo Xin <xin.guo@after615.com>
2016-09-13 15:35:58 +0800

    ruby运算符

--- ruby/rubyobj.rb
+++ ruby/rubyobj.rb
@@ -17,8 +17,21 @@ puts(fetch_and_downcase(hash,2))
 
 #面向对象示例
 require "net/http"
-require "uri"
+require "uri" #URL：是URI标识符中某种特定种类的东西，是URI中的一种
 url=URI.parse("http://www.ruby-lang.org/ja/")
 http=Net::HTTP.start(url.host,url.port)
 doc=http.get(url.path)
+puts(url.scheme) #体系：URL的种类,使用哪中通讯协议
+puts(url.host)	 #主机名
+puts(url.port)	 #端口号
+puts(url.path)	 #路径：定位服务器上管理的文件
+puts(url.to_s)
 puts(doc)
+=begin
+#调用Net::HTTP#get方法，对象内部的处理有：
+1.使用主机名和端口号，与服务器建立通信（socket套接字）
+2.使用路径，创建代表请求信息的Net::HTTPRequest对象
+3.对套接字写入请求信息
+4.从套接字中读取数据，并将其保存到代表响应信息的Net::HTTPResponse对象中
+5.利用Net::HTTPResponse本身提供的功能，解析响应信息，提取文档部分并返回
+=end
