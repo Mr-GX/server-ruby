d6bad34309143e53ab2dcc3e6c6dad8120475773
GuoXin <xin.guo@after615.com>
2017-01-09 11:01:23 +0800

    add column to users table

--- rails/blog/db/schema.rb
+++ rails/blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129025608) do
+ActiveRecord::Schema.define(version: 20170109025727) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161129025608) do
     t.boolean  "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "mobile"
   end
 
 end
