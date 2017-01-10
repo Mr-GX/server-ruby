9de16cab8a91bcb1064c8be58d783f9e6abff8bf
GuoXin <xin.guo@after615.com>
2017-01-10 15:48:20 +0800

    user validates

--- rails/blog/app/models/user.rb
+++ rails/blog/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-	validates :mobile,uniqueness:{message:"mobile can not repeat!"}
+	validates :name,presence:{message:"姓名不能为空!"}
+	validates :mobile,presence:{message:"手机号不能为空!"},uniqueness:{message:"手机号已存在!"},format:{with: /\A((\+?86)|(\(\+86\)))?1\d{10}\z/,message: "手机号不合法!"}
+	validates :email,presence:{message:"邮箱不能为空!"},uniqueness:{message:"邮箱已存在!"},format:{with: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,message: "邮箱不合法!"}
+	validates :activated,presence:{message:"时间不能为空!"}
 	ADMIN_STATUS = {
       '是' => 1,
       '否' => 0,
