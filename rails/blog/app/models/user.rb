13e5c6ac76c2d9c4407e62ec298c49a25860a37a
GuoXin <xin.guo@after615.com>
2017-01-22 17:31:37 +0800

    add gem devise

--- rails/blog/app/models/user.rb
+++ rails/blog/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	validates :name,presence:{message:"姓名不能为空!"}
 	validates :mobile,presence:{message:"手机号不能为空!"},uniqueness:{message:"手机号已存在!"},format:{with: /\A((\+?86)|(\(\+86\)))?1\d{10}\z/,message: "手机号不合法!"}
 	validates :email,presence:{message:"邮箱不能为空!"},uniqueness:{message:"邮箱已存在!"},format:{with: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,message: "邮箱不合法!"}
--- rails/blog/config/routes.rb
+++ rails/blog/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   #resources 方法，可以声明一个符合 REST 架构的资源。创建资源：指一系列类似的对象，使用rake routes生成path
   #对应ArticlesController和CommentsController，生成一些index/create/new/show/edit/update/destroy方法
