4b8b5632c1473c02811a0b06f8fcf9380a41acea
xin.guo <xin.guo@after615.com>
2018-04-17 17:08:31 +0800

    use bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+SERVER_NAME = "our zone"
+SERVER_DESCRIPTION = "网站介绍"
+SERVER_KEY_WORDS = "网站检索关键字"
