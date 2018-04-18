3ba720553a114c786295cc47e29d4cb454b613e3
xin.guo <xin.guo@after615.com>
2018-04-18 18:39:39 +0800

    home about

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users , controllers: {registrations: 'users/registrations',sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #设置程序首页
+  resources :homes, path:"/", only:[] do
+    collection do
+      get :about
+    end
+  end
+  get "/about" => "homes#about"
   root 'homes#index'	
 end
