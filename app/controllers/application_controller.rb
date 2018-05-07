9feb2674194ed9836a6d60e2a910a313792b9352
xin.guo <xin.guo@after615.com>
2018-05-07 18:52:39 +0800

    admin&user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
     @per_page = params[:per_page].present? ? (params[:per_page].to_i) : 10
     @page = params[:page].present? ? params[:page] : 1
   end
+
+  def authenticate_user!
+    session[:return_to] = request.url
+    super
+  end
+  
+  def authenticate_admin
+    # current_user.user_type == USER_TYPE_SUPER
+    unless user_signed_in?
+      render "admin/homes/index"
+    end
+  end
 end
