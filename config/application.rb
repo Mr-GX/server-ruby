4fe123dcae4da51bc939aaa06859dbe944d1b879
guoxin <g71x88@gmail.com>
2018-06-08 16:04:22 +0800

    timezone config

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,7 @@ module OurZone
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  config.active_record.default_timezone = :local 
+  config.time_zone = "Beijing"
 end
