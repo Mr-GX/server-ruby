6e4bdb66708f12e2e00732765652a5244fa69064
guoxin <g71x88@gmail.com>
2018-08-23 18:38:02 +0800

    page 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :add_default_params
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  rescue_from ActionController::RoutingError, :with => :render_404
 
   def add_default_params
     @per_page = params[:per_page].present? ? (params[:per_page].to_i) : 10
@@ -16,5 +18,13 @@ class ApplicationController < ActionController::Base
     unless user_signed_in? && current_user.user_type == USER_TYPE_ADMIN
       redirect_to root_path
     end
+  end
+
+  private
+  def render_404(exception = nil)
+    if exception
+        logger.info "Rendering 404: #{exception.message}"
+    end
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
 end
